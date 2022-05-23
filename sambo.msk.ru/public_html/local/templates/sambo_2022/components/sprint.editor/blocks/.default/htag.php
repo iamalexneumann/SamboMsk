6fbb21dd8e2675a98fff758f909063a62f18be4c
iamalexneumann <iamalexneumann@yandex.ru>
2022-05-23 14:29:49 +0300

    Добавляет clearfix для шаблона блока htag компонента sprint.editor

--- sambo.msk.ru/public_html/local/templates/sambo_2022/components/sprint.editor/blocks/.default/htag.php
+++ sambo.msk.ru/public_html/local/templates/sambo_2022/components/sprint.editor/blocks/.default/htag.php
@@ -1,1 +1,2 @@
+<div class="clearfix"></div>
 <<?= $block['type'] ?><?php if (!empty($block['anchor'])): ?> id="<?= $block['anchor']; ?>"<?php endif; ?>><?= $block['value']; ?></<?= $block['type']; ?>>
