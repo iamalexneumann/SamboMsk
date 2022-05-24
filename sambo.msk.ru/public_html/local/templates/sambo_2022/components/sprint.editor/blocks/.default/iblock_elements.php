d77e785f1ba782053c152848ae095a922187dfd2
iamalexneumann <iamalexneumann@yandex.ru>
2022-05-24 20:09:40 +0300

    Добавляет шаблон для блока Элементы компонента sprint.editor

--- sambo.msk.ru/public_html/local/templates/sambo_2022/components/sprint.editor/blocks/.default/iblock_elements.php
+++ sambo.msk.ru/public_html/local/templates/sambo_2022/components/sprint.editor/blocks/.default/iblock_elements.php
@@ -1,15 +1,20 @@
-<? /** @var $block array */ ?><?
+<?php /** @var $block array */ ?>
+<?php
 $elements = Sprint\Editor\Blocks\IblockElements::getList(
-    $block, [
-    'NAME',
-    'DETAIL_PAGE_URL',
-]
+    $block,
+    [
+        'NAME',
+        'DETAIL_PAGE_URL',
+    ]
 );
 ?>
-<div class="sp-iblock-elements">
-    <? foreach ($elements as $aItem): ?>
-        <div>
-            <a href="<?= $aItem['DETAIL_PAGE_URL'] ?>"><?= $aItem['NAME'] ?></a> <br/>
-        </div>
-    <? endforeach; ?>
+<div class="mt-5">
+    <div class="h2">Статьи по этой теме:</div>
+    <ul class="custom-ul-list">
+        <?php foreach ($elements as $aItem): ?>
+        <li>
+            <a href="<?= $aItem['DETAIL_PAGE_URL'] ?>"><?= $aItem['NAME'] ?></a>
+        </li>
+        <?php endforeach; ?>
+    </ul>
 </div>
