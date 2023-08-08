47eb3c99ff2135e2176ea046f061ac908e56f657
iamalexneumann <iamalexneumann@yandex.ru>
2023-08-08 18:09:34 +0300

    Add the link to the comagic function

--- sambo.msk.ru/public_html/local/php_interface/include/functions.php
+++ sambo.msk.ru/public_html/local/php_interface/include/functions.php
@@ -60,6 +60,7 @@ function use_comagic (string $url):bool
         '#^/nashi-zaly/sambo-i-dzyudo-v-shcherbinke/$#',
         '#^/nashi-zaly/sambo-i-dzyudo-v-moskovskom/$#',
         '#^/nashi-zaly/sambo-i-dzyudo-v-fili-davydkovo/$#',
+        '#^/nashi-zaly/sambo-i-dzyudo-v-moskovskom-3-y-mikrorayon/$#',
     ];
     for ($i = 0; $i < count($patterns); $i++) {
         preg_match($patterns[$i], $url, $matches);
